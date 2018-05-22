f187221eaebd379d9e7b45cb7340029cb43edf4d
LoeiFy <LoeiFy@gmail.com>
2018-05-22 20:38:32 +0800

    vue

--- src/component/babel.js
+++ src/component/babel.js
@@ -1,12 +1,19 @@
+import $ from './query.js'
 const { Babel } = window
+
+Babel.registerPlugin('transform-vue-jsx', window['babel-plugin-transform-vue-jsx'])
 
 export default function (js) {
   const presets = ['es2015', 'react']
   const plugins = [
     'transform-es2015-modules-umd',
     'transform-class-properties',
+    'transform-decorators-legacy',
   ]
-  const { code } = Babel.transform(js, { presets, plugins })
 
-  return code
+  if ($('#vue').context.checked) {
+    plugins.push('transform-vue-jsx')
+  }
+
+  return Babel.transform(js, { presets, plugins }).code
 }
