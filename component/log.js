b34cf4513dd124ef0d21888e119f7a8c3b14b41c
LoeiFy <LoeiFy@gmail.com>
2018-05-31 10:07:05 +0800

    log style

--- component/log.js
+++ component/log.js
@@ -2,6 +2,9 @@ export default function (type, data) {
   const time = new Date().toLocaleTimeString().split(' ')[0]
 
   return `
-    <p class="${type}"><span>${time}</span>${data}</p>
+    <p class="${type}">
+      <span>${time}</span>
+      <span>${data}</span>
+    </p>
   `
 }
