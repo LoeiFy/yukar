79805f9fa98415711e428b543674f76fb3fc9de2
LoeiFy <LoeiFy@gmail.com>
2018-06-02 16:24:16 +0800

    object log

--- component/log.js
+++ component/log.js
@@ -1,10 +1,23 @@
 export default function (type, data) {
   const time = new Date().toLocaleTimeString().split(' ')[0]
+  const message = []
+
+  data.forEach((m) => {
+    if (typeof m === 'string') {
+      message.push(m)
+      return
+    }
+    try {
+      message.push(JSON.stringify(m))
+    } catch (e) {
+      message.push(m)
+    }
+  })
 
   return `
     <p class="${type}">
       <span>${time}</span>
-      <span>${data}</span>
+      <span>${message.join(' ')}</span>
     </p>
   `
 }
