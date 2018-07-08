dcb457140c662b64ae278ff60b46cc9519ecc9b6
LoeiFy <LoeiFy@gmail.com>
2018-07-08 21:06:59 +0800

    check umd transform

--- component/babel.js
+++ component/babel.js
@@ -1,18 +1,22 @@
 const { Babel } = window
-
-Babel.registerPlugin('transform-vue-jsx', window['babel-plugin-transform-vue-jsx'])
+const requireRegex = /(\brequire\s*?\(\s*?)(['"])([^'"]+)(\2\s*?\))/g
+const importRegex = /\bimport\s.*/g
 
 export default function (js, vue) {
   const presets = ['react']
   const plugins = [
-    'transform-es2015-modules-umd',
     'transform-class-properties',
     'transform-decorators-legacy',
     'transform-object-rest-spread',
   ]
 
   if (vue) {
+    Babel.registerPlugin('transform-vue-jsx', window['babel-plugin-transform-vue-jsx'])
     plugins.push('transform-vue-jsx')
+  }
+
+  if (requireRegex.test(js) || importRegex.test(js)) {
+    plugins.push('transform-es2015-modules-umd')
   }
 
   return Babel.transform(js, { presets, plugins }).code
